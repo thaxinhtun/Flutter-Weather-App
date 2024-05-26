122bd59964262028c194f15644d0a445f9e09ceb
Thazin Htun <thaxinhtun@gmail.com>
2024-05-26 13:25:32 +0630

    commit

--- lib/util/style.dart
+++ lib/util/style.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String apiKey = "b4c376c795fda8abd888cab8c9c94d74";
+// constant style widgets.
 
 Widget kCustomText(String text, double size, FontWeight weight) {
   return Text(
